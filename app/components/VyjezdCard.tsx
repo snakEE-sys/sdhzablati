1d41082c9db49d796304ec11bb1697a8460130da
Andrzej Grabowski <dev@andrzejgrabowski.cz>
2025-12-16 02:17:53 +0100

    fixed issues

--- app/components/VyjezdCard.tsx
+++ app/components/VyjezdCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Card, CardContent } from "@/components/ui/card"
-import { getVyjezd } from "@/utils/db";
+
 
 interface VyjezdyItem {
     id: string
@@ -14,7 +14,6 @@ interface VyjezdyItem {
 
 export default async function Vyjezdy() {
     //call function getData to get vyjezdy
-    const vyjezdy = await getData();
 
     // Format date
     const formatDate = (dateString: Date) => {
@@ -30,14 +29,14 @@ export default async function Vyjezdy() {
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-3xl font-bold mb-6">Vyjezdy</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {vyjezdy.map((item) => (
+                {/*vyjezdy.map((item) => (
                     <VyjezdyCard key={item.id} item={item} formatDate={formatDate} />
-                ))}
+                ))*/}
             </div>
         </div>
     )
 }
-
+/*
 async function getData() {
     try {
         const data = await getVyjezd();
@@ -57,7 +56,7 @@ async function getData() {
         return null;
     }
 }
-
+*/
 function VyjezdyCard({
                          item,
                          formatDate,
