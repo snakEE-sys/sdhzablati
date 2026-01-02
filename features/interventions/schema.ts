527f464ed91689c3480a2a2ab04dc6bc47bf823f
Andrzej Grabowski <dev@andrzejgrabowski.cz>
2026-01-02 21:39:10 +0100

    add zod validation schema

---
+++ features/interventions/schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod/v4";
+
+export const InterventionSchema = z.object({
+  category: z.string().min(1, "Kategorie je povinná"),
+  subcategory: z.string().min(1, "Podkategorie je povinná"),
+  date: z.date({
+    error: "Datum je povinné",
+  }),
+  time: z.iso.time({
+    error: "Čas je povinný",
+  }),
+  address: z.string().min(5, "Adresa je povinná"),
+  description: z.string().min(50, "Popis musí mít alespoň 50 znaků"),
+  deployment: z
+    .array(
+      z.object({
+        unitId: z.uuid("Neplatné ID jednotky"),
+        unitName: z.string(),
+        vehicleId: z.uuid("Neplatné ID vozidla"),
+        vehicleName: z.string(),
+        quantity: z.int().min(1),
+      })
+    )
+    .min(1, "Musíte přidat alespoň jednu techniku"),
+  images: z.array(z.string().optional()),
+});
