75029eb9206d97889d74d7aea1f821f974def324
Andrzej Grabowski <dev@andrzejgrabowski.cz>
2026-01-04 15:52:17 +0100

    fix: wrong select and selectContent imports

--- components/form/SelectField.tsx
+++ components/form/SelectField.tsx
@@ -1,10 +1,17 @@
-import { SelectContent, SelectItem } from "@radix-ui/react-select";
+"use client";
 import { useFieldContext } from ".";
 import { Field, FieldContent, FieldDescription, FieldLabel } from "../ui/field";
-import { Select, SelectTrigger, SelectValue } from "../ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 import { FieldErrors } from "./FieldErrors";
 
 type DataProps = {
+  id: string;
   name: string;
 };
 type SelectFieldProps = {
@@ -21,29 +28,29 @@ export function SelectField({ data, label, desc }: SelectFieldProps) {
     <Field>
       <FieldContent data-invalid={isInvalid}>
         <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+        <Select
+          name={field.name}
+          value={field.state.value}
+          onValueChange={field.handleChange}
+        >
+          <SelectTrigger
+            id={field.name}
+            aria-invalid={isInvalid}
+            className="min-w-[180px]"
+          >
+            <SelectValue placeholder="Vybrat" />
+          </SelectTrigger>
+          <SelectContent position="popper">
+            {data.map((d) => (
+              <SelectItem key={d.id} value={d.name}>
+                {d.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
         <FieldDescription>{desc}</FieldDescription>
         <FieldErrors meta={field.state.meta} />
       </FieldContent>
-      <Select
-        name={field.name}
-        value={field.state.value}
-        onValueChange={field.handleChange}
-      >
-        <SelectTrigger
-          id={field.name}
-          aria-invalid={isInvalid}
-          className="min-w-[120px]"
-        >
-          <SelectValue placeholder="Vybrat" />
-        </SelectTrigger>
-        <SelectContent position="item-aligned">
-          {data.map((d) => (
-            <SelectItem key={d.name} value={d.name}>
-              {d.name}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
     </Field>
   );
 }
